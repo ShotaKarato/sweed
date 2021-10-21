2e81ebda7622c72ba7e6c655e1eb45a4c310dc16
ShotaKarato <theseuta@gmail.com>
2021-10-21 20:50:15 +0900

    update: fixed style and added navigation toggle function

--- js/index.js
+++ js/index.js
@@ -1,14 +1,44 @@
-const memberNames = document.querySelectorAll('.member-names__img');
-const memberBubbles = document.querySelectorAll('.member-bubbles__bubble');
+// function: toggle nav
+const navLink = document.querySelector(".links__nav-link");
+const nav = document.querySelector(".nav");
+const navCloseIcon = document.querySelector(".nav__icon-close");
 
-memberNames.forEach(name => {
-  name.addEventListener('click', (e) => {
+const toggleNav = () => {
+  nav.classList.toggle("hidden");
+  document.body.classList.toggle("no-scroll");
+};
+
+navLink.addEventListener("click", () => {
+  toggleNav();
+});
+navCloseIcon.addEventListener("click", () => {
+  toggleNav();
+});
+const links = document.querySelectorAll(".nav__menu-link");
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    toggleNav();
+  });
+});
+
+// function: toggle speech bubble
+const memberNames = document.querySelectorAll(".member-names__img");
+const memberBubbles = document.querySelectorAll(".member-bubbles__bubble");
+
+memberNames.forEach((name) => {
+  name.addEventListener("click", (e) => {
     const memberName = e.currentTarget.dataset.id;
     const selectedMember = document.getElementById(memberName);
 
-    memberBubbles.forEach(bubble => {
-      bubble.classList.add('hidden');
+    // add underline for selected user
+    memberNames.forEach((name) => {
+      name.classList.remove("member-names__img--selected");
     });
-    selectedMember.classList.remove('hidden');
+    e.currentTarget.classList.add("member-names__img--selected");
+    // display speech bubble
+    memberBubbles.forEach((bubble) => {
+      bubble.classList.add("hidden");
+    });
+    selectedMember.classList.remove("hidden");
   });
 });
